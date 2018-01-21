0ea258b6c684b5b8b41617ea25bdb4293778cd04
hadu <hadu@github.com>
2018-01-21 17:47:00 -0500

    add gitignore

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 const express = require( 'express' );
 const volleyball = require('volleyball');
+const nunjucks = require('nunjucks');
 
 const app = express();
+
+//app.engine (docs) to use nunjucks.render as the function to actually render html
+//app.set (docs) the default view engine to html (so we don't have to specify on every render)
+//nunjucks.configure (docs) the views path to our views folder (where we store our templates)
+app.set('view engine', 'html'); 
+app.engine('html', nunjucks.render);
+
+nunjucks.configure('views', { noCache: true });
+
+var locals = {
+    title: 'An Example',
+    people: [
+        {name: 'Gandalf'},
+        {name: 'Frodo' },
+        {name: 'Hermione'}
+    ]
+};
+
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
+nunjucks.render('index.html', locals, function (err, output) {
+    console.log('output', output);
+    console.log('err', err);
+});
 
 //create some logging middleware that will fire for every incoming request
 app.use(volleyball);
@@ -13,7 +38,7 @@ app.use('/special/',function (req, res, next) {
     next();
 })
 
-app.get('/', (req, res) => res.send('HOME')) //localhost:3000
+app.get('/', (req, res) => res.render( 'index', {title: 'Hall of Fame', people: people})) //localhost:3000
 
 app.get('/News', (req, res) => res.send('NEWS')) //localhost:3000/News
 
